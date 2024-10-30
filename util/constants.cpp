865ac38f461c948e6791b8be861ce3fa31fca3cc
Aryan Bodhe <aryanbodhe7705@gmail.com>
2024-10-30 09:32:23 +0530

    updated constants

--- util/constants.cpp
+++ util/constants.cpp
@@ -1,2 +1,13 @@
 #define DIMENSION 2
 #define LEAF_POINT_COUNT_THRESHOLD 5
+#define BLOBS_2D "Datasets/csv/2D_Blobs.csv"
+#define MOONS_2D "Datasets/csv/2D_Moons.csv"
+#define BLOBS_3D "Datasets/csv/3D_Blobs.csv"
+#define S_CURVE_3D "Datasets/csv/3D_S_Curve.csv"
+#define SWISS_ROLL_3D "Datasets/csv/3D_Swiss_Roll.csv"
+#define BLOBS_5D "Datasets/csv/5D_Blobs.csv"
+#define BLOBS_10D "Datasets/csv/10D_Blobs.csv"
+#define LATIN_CENTER_DATASET "Datasets/csv/latin_center_data.csv"
+#define HIGHLEYMAN_DATASET "Datasets/csv/highleyman_data.csv"
+#define LITHUANIAN DATASET "Datasets/csv/lithuanian_data.csv"
+#define SOBOL_DATASET "Datasets/csv/sobol_data.csv"
